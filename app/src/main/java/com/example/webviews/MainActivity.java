da0216582e84896aead5e9dbeb489141fd36324e
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-06-09 19:40:19 +0500

    update buttons

--- app/src/main/java/com/example/webviews/MainActivity.java
+++ app/src/main/java/com/example/webviews/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.webviews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +15,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = findViewById(R.id.webView);
+        final WebView webView = findViewById(R.id.webView);
+        Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+
 
         webView.getSettings().setJavaScriptEnabled(true);
 
         webView.setWebViewClient(new WebViewClient());
 
-        //webView.loadUrl("http://www.google.com");
+        webView.loadUrl("https://www.google.com/");
 
-        webView.loadData("<hmtl><body><h1>Hello world!</h1><p>THis is my cool website!</p></body></html>","text/html","UTF-8");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                webView.loadData("<hmtl><body><h1>Hello world!</h1><p>THis is my cool website!</p></body></html>","text/html","UTF-8");
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                webView.loadUrl("https://www.google.com/");
+
+            }
+        });
+
+
     }
 }
